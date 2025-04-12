50442b706da11270e9c566f979c53d2ea59a4f0b
SMUZhanLi <2194996299@qq.com>
2025-04-12 22:55:14 +0800

    rm python3 build files

--- build/lib/biomdadbapi/utilities.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-# @File: utilities.py
-
-def row_to_dict(tbl, sql):
-    row2dict = [{column.name: getattr(row, column.name) for column in
-                 tbl.__table__.columns} for row in sql]
-    return row2dict
